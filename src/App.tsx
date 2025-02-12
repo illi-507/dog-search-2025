d95380e22ba949c25251c68653438e75c991aa7e
illi-510 <xingjian@adsgency.ai>
2025-02-11 18:52:59 -0800

    feat: fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./component/Login";
 import DogSearch from "./component/DogSearch";
 import FavoriteList from "./component/FavoriteList";
